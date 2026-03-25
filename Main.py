e7d58febd4072923636054df8c5530493dc006f7
hazemsatr4-rgb <hazemsatr4@gmail.com>
2026-03-25 10:19:44 +0300

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,9 @@
+"""
+Toolmaker OS - Main Entry Point
+Run any tool from the workbench directory.
+
+Usage:
+    python3 workbench/link_checker_v1.py
+    python3 workbench/project_exporter.py
+"""
+print("Toolmaker OS ready. Run tools from the workbench/ directory.")
